4a8348f14937f3beefd21ab90269264209bf8207
Patrick M <Patrick.M.Professional@gmail.com>
2016-02-26 15:20:29 -0500

    most of the bare bones file structure implements. do not see empty dirs, so commiting to see if they stuck.

---
+++ routes/genres.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var db = require('../db/genre_fns');
--- server.js
+++ server.js
@@ -1,43 +1,51 @@
+//for debugging
 pry = require('pryjs');
-/*global require __dirname process*/
+
 
 'use strict';
 
+//require dotenv when in development environment
 if(!process.env.NODE_ENV){
 require('dotenv').config();
 }
 
+//required stuff/dependencies
 var path = require('path');
 var express = require('express');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
-var massagesRouter = require('./routes/massages');
-var masseuistsRouter = require('./routes/masseuists');
+var booksRouter = require('./routes/books');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
+//determine logger version
 if( process.env.NODE_ENV === 'development' ){
   app.use(morgan('dev'));
 } else {
   app.use(morgan('common'));
 }
 
+//parseyness
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//allow more than get or post
 app.use(methodOverride('_method'));
 
+//set view stuff
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//render homepage
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.use('/massages', massagesRouter);
-app.use('/masseuists', masseuistsRouter);
+app.use('/books', booksRouter);
+app.use('/users', usersRouter);
 
 
 app.listen(process.env.PORT, function() {
