f9b437a917105c77557e5e433c2f90c75c3503c7
Patrick M <Patrick.M.Professional@gmail.com>
2016-02-29 11:02:25 -0500

    login and logout functional.

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db/user_fns');
 
-var users = express.Router();
+// var users = express.Router();
 var bodyParser = require('body-parser'); //i think this is unnecessary since it's in server.js, but following tutorial.
 // var session = require('express-session');
 // var pgSession = require('connect-pg-simple')(session);
@@ -35,8 +35,11 @@ router
   .get('/login', (req,res) => {
     res.render('users/login')
   })
-  .post('/login', db.loginUser, (res, req) => {
-    console.log(res.rows, 'that was res.rows in users_fns.js'); //res.rows returns undefined
+
+router
+  .post('/login', db.loginUser, (req, res) => {
+    console.log(res.rows, 'that was res.rows in users_fns.js');
+    //res.rows returns undefined
     req.session.user = res.rows;
     // when you redirect you must force a save due to asynchronisity
     // https://github.com/expressjs/session/issues/167 **
@@ -49,7 +52,12 @@ router
 router.post('/', /*getusersfromSQL, */ notImplement)
 
 //delete
-router.delete('/', /*getusersfromSQL, */ notImplement )
+router.delete('/logout', (req,res) => {
+  req.session.destroy( function(err) {
+    res.redirect(303,'/')
+  } )
+  console.log('logout button was pressed/ .delete route to /logout called');
+} );
 
 //put???
 router.put('/', /*getusersfromSQL, */ notImplement )
