5fe9e58157e7f5508004196f533a8e6b6ac9bedf
Patrick M <Patrick.M.Professional@gmail.com>
2016-02-28 19:28:42 -0500

    users can now create accounts.

--- db/user_fns.js
+++ db/user_fns.js
@@ -1,6 +1,10 @@
 'use strict';
 
+//dependencies
 var pg = require('pg');
+var bcrypt = require('bcrypt');
+
+var salt = bcrypt.genSaltSync(10); //i think this is where colin meant when referring to 'to the top'
 
 // var config = {
 //   host: process.env.DB_HOST,
@@ -30,3 +34,38 @@ module.exports.getUsers = (req, res, next) => {
     });
   });
 };
+
+function createSecure(email, password, callback){
+  //has password entered at signup
+  bcrypt.genSalt( (err,salt) => {
+    bcrypt.hash(password, salt, (err,hash) => {
+      //this callback saves the user to DB with hash'd pw
+      callback(email, hash);
+    });
+  });
+};
+
+module.exports.createUser = (req, res, next) => {
+  createSecure(req.body.email, req.body.password, saveUser);
+
+  function saveUser(email, hash) {
+    // Get a Postgres client from the connection pool
+    pg.connect(DB_config, function(err, client, done) {
+      // Handle connection errors
+      if(err) {
+        done();
+        console.log(err);
+        return res.status(500).json({ success: false, data: err});
+      }
+
+      var query = client.query("INSERT INTO users (email, password_digest, first_name, last_name, zip) VALUES ($1, $2, $3, $4, $5);",
+        [email, hash, req.body.first_name, req.body.last_name, req.body.zip], function(err, result) {
+          done()
+          if(err) {
+            return console.error('error, running query', err);
+          }
+          next()
+      });
+    });
+  }
+}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db/user_fns');
 
+var users = express.Router();
+var bodyParser = require('body-parser'); //i think this is unnecessary since it's in server.js, but following tutorial.
 
 
 
@@ -16,6 +18,15 @@ router
   .get('/', db.getUsers, (req,res) => {
     res.render('users/uindex', {users:res.users})
   })
+
+//user signup
+router
+  .get('/new_user', /*db.newUser,*/ (req,res) => {
+    res.render('users/new_user_signup')
+  } )
+  .post('/', db.createUser, (req, res) => {
+    res.redirect(303,'/');
+  }) //works. redirects to 'index' page
 
 //post
 router.post('/', /*getusersfromSQL, */ notImplement)
